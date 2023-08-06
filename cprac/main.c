14bf1e6f8fa9cd1c2ee0cede25d67ac17b428682
Toufikur Rahman <toufiikur@gmail.cm>
2023-08-06 12:28:43 +0600

    Initial Commit

---
+++ cprac/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  cprac
+//
+//  Created by Toufikur Rahman on 8/6/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
